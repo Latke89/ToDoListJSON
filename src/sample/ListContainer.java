8527ac59e1bedf86c2548a9a821d85ab5b0b6f65
Brett Gordon <gordonbrett89@gmail.com>
2016-09-02 08:05:05 -0400

    text fix

--- src/sample/ListContainer.java
+++ src/sample/ListContainer.java
@@ -1,12 +1,11 @@
 package sample;
 
 import java.util.ArrayList;
-import java.io.Serializable;
 
 /**
  * Created by Brett on 8/31/16.
  */
-public class ListContainer implements Serializable{
+public class ListContainer{
 
 
 
